d431e1be9e4c4285f63dcc4e6569838cdc98a095
stanleywanjau <stanleywanjau371@gmail.com>
2023-12-17 08:05:20 +0300

    completed

--- lib/debug.py
+++ lib/debug.py
@@ -2,7 +2,35 @@
 
 from config import CONN, CURSOR
 from song import Song
+# import pytest; pytest.set_trace()
+
 
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+#  with CONN:
+    Song.create_table()
+    
+    
+    
+    CURSOR.execute("PRAGMA table_info(songs)").fetchall()
+    
+    
+    hello = Song("Hello", "25")
+    hello.save()
+
+    despacito = Song("Despacito", "Vida")
+    despacito.save()
+
+    hello.id
+# => 1
+    despacito.id
+
+    CONN.commit()
+
+        # Fetch and print all songs
+    songs = CURSOR.execute('SELECT * FROM songs').fetchall()
+    print("All Songs:")
+    for song in songs:
+     print(song)
+   
+  
--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,39 @@
-from config import CONN, CURSOR
+# File: song.py
+
+import sqlite3
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @staticmethod
+    def create_table():
+        with sqlite3.connect("music.db") as connection:
+            cursor = connection.cursor()
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS songs (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT,
+                    album TEXT
+                )
+            ''')
+
+    def save(self):
+        with sqlite3.connect("music.db") as connection:
+            cursor = connection.cursor()
+
+            
+            cursor.execute('''
+                INSERT INTO songs (name, album)
+                VALUES (?, ?)
+            ''', (self.name, self.album))
+            self.id = cursor.lastrowid
+
+
+    @classmethod
+    def create(cls, name, album):
+        song = cls(name, album)
+        song.save()
+        return song
